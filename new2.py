7b39072b98e0644195beb78a49f79545ee88b2bd
petercsnk <peter.ch.snook@gmail.com>
2026-02-26 16:09:37 +0000

    add view func & move deleted to bin

--- new2.py
+++ new2.py
@@ -1,18 +1,5 @@
-import csv
 import os
-import numpy as np
 import pandas as pd
-
-
-cwd = os.path.dirname(os.path.abspath(__file__))
-groupsFile = os.path.join(cwd, "groups.csv")
-exercisesFile = os.path.join(cwd, "exercises.csv")
-
-
-# def appendCSV(filename, data):
-#     with open(filename, "a+", newline="") as f:
-#         writer = csv.writer(f)
-#         writer.writerow(data)
 
 
 def writeCSV(filename, df):
@@ -20,67 +7,146 @@ def writeCSV(filename, df):
 
 
 def readCSV(filename):
-    df = pd.read_csv(filename)
-    return df
+    return pd.read_csv(filename)
 
 
-def createConfig():
-    if not os.path.isfile(groupsFile):
+def init():
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    data = os.path.join(cwd, "data")
+    bin = os.path.join(cwd, "bin")
+    config = os.path.join(cwd, "config")
+    for directory in [data, bin, config]:
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+    groups = os.path.join(config, "groups.csv")
+    exercises = os.path.join(config, "exercises.csv")
+    if not os.path.isfile(groups):
         df = pd.DataFrame(columns=["name"])
-        writeCSV(groupsFile, df)
-    if not os.path.isfile(exercisesFile):
+        writeCSV(groups, df)
+    if not os.path.isfile(exercises):
         df = pd.DataFrame(columns=["name", "maxSets"])
-        writeCSV(exercisesFile, df)
+        writeCSV(exercises, df)
+    return data, bin, groups, exercises
 
 
-def inputGroupName():
-    return input("Group Name: ")
+def inputName():
+    return input("Name: ")
 
 
 def inputChoice():
-    return int(input("Selection: "))
+    choice = input("Selection: ")
+    try:
+        return int(choice)
+    except ValueError:
+        return 0
+
+
+def doesNameExist(df, name):
+    if name in df["name"].values:
+        return True
+    else:
+        return False
 
 
 def inputManagerGroups(df):
-    df = df
-    print(
-        """
-        Create Group (1)
-        Delete Group (2)
-        Rename Group (3)
-        Save (4)
-        """
-    )
- 
     while True:
-        print(df)
+        print(
+            """
+            Group Manager
+            Create  (1)
+            Delete  (2)
+            Rename  (3)
+            Save    (4)
+            """
+        )
         choice = inputChoice()
+        if choice in [1, 2, 3]:
+            name = inputName()
         match choice:
             case 1:
-                name = inputGroupName()
-                df.loc[len(df)] = [name]
+                if not doesNameExist(df, name):
+                    df.loc[len(df)] = name
+                else:
+                    print("Already Exists")
             case 2:
-                name = inputGroupName()
-                df.drop(df.loc[df["name"] == name].index, inplace=True)
+                if doesNameExist(df, name):
+                    df.drop(df.loc[df["name"] == name].index, inplace=True)
+                else:
+                    print("Does Not Exist")
             case 3:
-                pass
+                if doesNameExist(df, name):
+                    new = input("New Name: ")
+                    df.loc[df["name"] == name] = new
+                else:
+                    print("Does Not Exist")
             case 4:
-                writeCSV(groupsFile, df)
-                return
+                return df
             case _:
                 print("Invalid")
-                
 
-def inputManager():
-    print("Configure Groups (1)")
+
+def createTracker(name, maxSets, directory):
+    headers = ["exercise", "date"]
+    for num in range(maxSets):
+        numStr = str(num + 1)
+        headers.append("repsSet" + numStr)
+        headers.append("weightSet" + numStr)
+    df = pd.DataFrame(columns=headers)
+    filename = os.path.join(directory, name + ".csv")
+    writeCSV(filename, df)
+
+
+def inputManagerExercises(df, data, bin):
+    while True:
+        print(
+            """
+            Exercise Manager
+            Create  (1)
+            Delete  (2)
+            Save    (3)
+            """
+        )
+        choice = inputChoice()
+        if choice in [1, 2]:
+            name = inputName()
+        match choice:
+            case 1:
+                if not doesNameExist(df, name):
+                    maxSets = int(input("Max Sets: "))
+                    df.loc[len(df)] = [name, maxSets]
+                    createTracker(name, maxSets, data)
+                else:
+                    print("Already Exists")
+            case 2:
+                if doesNameExist(df, name):
+                    df.drop(df.loc[df["name"] == name].index, inplace=True)
+                else:
+                    print("Does Not Exist")
+            case 3:
+                return df
+            case _:
+                print("Invalid")
+
+
+def inputManagerMain(data, bin, groups, exercises):
+    print(
+        """
+        Configure Groups (1)
+        Configure Exercises (2)
+        """
+    )
     choice = inputChoice()
     match choice:
         case 1:
-            df = readCSV(groupsFile)
-            inputManagerGroups(df)
-
+            df = readCSV(groups)
+            df2 = inputManagerGroups(df)
+            writeCSV(groups, df2)
+        case 2:
+            df = readCSV(exercises)
+            df2 = inputManagerExercises(df, data, bin)
+            writeCSV(exercises, df2)
 
 
 if __name__ == "__main__":
-    createConfig()
-    inputManager()
+    data, bin, groups, exercises = init()
+    inputManagerMain(data, bin, groups, exercises)
